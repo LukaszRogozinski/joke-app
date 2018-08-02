b82cfd215b4db0decc27e42e49093376c31306fb
Lukasz Rogozinski <lukasz.rogozinski@mobica.com>
2018-08-02 12:53:59 +0200

    initialize project

---
+++ src/main/java/guru/springframework/model/ChuckNorris.java
@@ -0,0 +1,10 @@
+package guru.springframework.model;
+
+import guru.springframework.services.JokeService;
+
+public class ChuckNorris implements JokeService {
+    @Override
+    public String getRandomQuote() {
+        return null;
+    }
+}
